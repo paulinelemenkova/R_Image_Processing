6493817f48ed3b7d611adbb4c5f3ff49b67896e3
Pauline Lemenkova <pauline.lemenkova@gmail.com>
2022-11-20 17:28:17 +0100

    Add files via upload

--- Landsat_UC_k_means_RStoolbox_20112022.r
+++ Landsat_UC_k_means_RStoolbox_20112022.r
@@ -136,3 +136,35 @@ legend(x = "topleft", inset = 0.03, legend=c("1=Swamp forest", "2=Humid tropical
 
 #-------------------- Basoko-2022: END-----------------#
 
+#-------------------- Kisangani-2013: START-----------------#
+# Set up working directory
+setwd("/Users/polinalemenkova/Documents/R/53_UC_k_means/Kisangani_LC08_L1TP_176060_20130413_20200913_02_T1")
+# Importing data
+Landsat_Kisangani2013 <- list.files("/Users/polinalemenkova/Documents/R/53_UC_k_means/Kisangani_LC08_L1TP_176060_20130413_20200913_02_T1")
+# Printing the list
+list.files()
+#
+# Stacking the data to create a RasterStack. 
+Landsat_Kisangani2013_stack <- stack(Landsat_Kisangani2013)
+# Turning a stack into a brick. 
+Landsat_Kisangani2013_brick <- brick(Landsat_Kisangani2013_stack)
+# Viewing brick attributes
+Landsat_Kisangani2013_brick
+#
+# Running the classification
+set.seed(25)
+unC <- unsuperClass(Landsat_Kisangani2013_brick, nSamples = 100, nClasses = 10, nStarts = 5)
+unC
+#
+# Creating color palette
+#colors <- rainbow(10)
+colors <- brewer.pal(n = 11, name = 'Paired')
+#colors <- rev(brewer.pal(n = 10, name = 'Paired'))
+#colors <- jet(10)
+# plotting a map
+plot(unC$map, main = "K-means Clustering for Landsat-8 OLI/TIRS C1 image of Kisangani, Congo  \nLC08_L1TP_177059_20131216_20200912_02_T1 (2013)", font.main=1, cex.main = 0.85, col = colors, axes = FALSE, box = FALSE, legend = FALSE)
+# adding legend
+legend(x = "topleft", inset = 0.03, legend=c("1=Urban areas", "2=Humid tropical forest", "3=Secondary forest", "4=Inundated grassland", "5=Deciduous forest", "6=Shrubland and grassland", "7=Water", "8=Cropland and mosaic forest", "9=Swamp forest", "10=Savannah, sparse trees"), fill = colors, title = "LULC ID Classes", horiz = FALSE,  bty = "n", text.font=3, ncol=1)
+
+#-------------------- Basoko-2013: END-----------------#
+
