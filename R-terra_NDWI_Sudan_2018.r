6a591df74b36c585440a224b1a025e3bccc4433a
Pauline Lemenkova <pauline.lemenkova@gmail.com>
2023-02-09 19:22:14 +0100

    Add files via upload

---
+++ R-terra_NDWI_Sudan_2018.r
@@ -0,0 +1,30 @@
+# Computing vegetation indices
+library(terra)
+library(RColorBrewer)
+library(Hmisc)
+library(pals)
+#
+setwd("/Users/polinalemenkova/Documents/R/52_Image_Processing/Sudan_NDWI")
+# Normalized Difference Water Index (NDWI) = (NIR-SWIR1) / (NIR+SWIR1)
+# Nir = Band 5, SWIR1 = Band 6.
+vi <- function(img, k, i) {
+  bk <- img[[k]]
+  bi <- img[[i]]
+  vi <- (bi - bk) / (bi + bk)
+  return(vi)
+}
+# For Landsat NIR = 5, SWIR1 = Band 6.
+filenames <- paste0('LC08_L2SP_173049_20181218_20200830_02_T1_SR_B', 1:7, ".tif")
+filenames
+landsat <- rast(filenames)
+landsat
+ndwi <- vi(landsat, 5, 6)
+options(scipen=10000)
+colors <- jet(100)
+plot(ndwi, col=colors, font.main = 1, main = "NDWI for Landsat-8 OLI/TIRS C1 image LC08_L2SP_173049_20181218_20200830_02_T1_SR_B: \nWhite Nile and Blue Nile confluence, Sudan (2018)", cex.main=0.9)
+minor.tick(nx = 10, ny = 10, tick.ratio = 0.3)
+# Plotting histogram of the NDVI
+hist(ndwi, font.main = 1, main = "NDWI values for Landsat-8 OLI/TIRS C1 image \nLC08_L2SP_173049_20181218_20200830_02_T1_SR_B: White Nile and Blue Nile confluence, Sudan (2018)", xlab = "RECI", ylab= "Frequency",
+    col = "mediumpurple1", xlim = c(-0.5, 1),  breaks = 50, xaxt = "n")
+axis(side=1, at = seq(-0.6, 1, 0.1), labels = seq(-0.6, 1, 0.1))
+minor.tick(nx = 10, ny = 10, tick.ratio = 0.3)
