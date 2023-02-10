0c2eefee5f1ea8062af76fe72977186e19b6c3d5
Pauline Lemenkova <pauline.lemenkova@gmail.com>
2023-02-10 17:01:18 +0100

    Add files via upload

--- R-terra_NDWI_Sudan_2022.r
+++ R-terra_NDWI_Sudan_2022.r
@@ -5,33 +5,28 @@ library(Hmisc)
 library(pals)
 #
 setwd("/Users/polinalemenkova/Documents/R/52_Image_Processing/Sudan_NDWI")
-# Normalized Difference Water Index (NDWI) = (Green-NIR) / (Green+NIR)
-# Nir = Band 5, Green = Band 3.
-vi <- function(img, k, i) {
+# Normalized Difference Water Index (NDWI) = (NIR - SWIR1) / (NIR + SWIR1)
+# NIR = Band 5, SWIR1 = Band 6
+vi <- function(img, i, k) {
   bk <- img[[k]]
   bi <- img[[i]]
-  vi <- (bk - bi) / (bk + bi)
+  vi <- (bi - bk) / (bi + bk)
   return(vi)
 }
-# For Landsat NIR = 5, red = 4.
+# For Landsat NIR = 5, SWIR1 = 6.
 filenames <- paste0('LC09_L2SP_173049_20221221_20221224_02_T1_SR_B', 1:7, ".tif")
 filenames
 landsat <- rast(filenames)
 landsat
-ndwi <- vi(landsat, 5, 3)
+ndwi <- vi(landsat, 5, 6)
+ndwi
 options(scipen=10000)
-colors <- jet(100)
-#colors <- kovesi.linear_bmy_10_95_c78(100)
-#colors <- brewer.piyg(100)
-#colors <- cubicl(100)
-#colors <- cols25(25)
-#colors <- polychrome(36)
-#colors <- glasbey(36)
-#colors <- kelly(22)
-#plot(ndvi, col=brewer.pal(100, "jet"), font.main = 1, main = "NDVI for Landsat-8 OLI/TIRS C1 image LC08_L2SP_197050_20131110_20200912_02_T1_SR: Inner Niger Delta, Mali (2013)", cex.main=0.9)
-plot(ndwi, col=colors, font.main = 1, main = "NDWI for Landsat-8 OLI/TIRS C1 image LC09_L2SP_173049_20221221_20221224_02_T1_SR_B: \nWhite Nile and Blue Nile confluence, Sudan (2022)", cex.main=0.9)
+#colors <- rev(jet(100))
+colors <- brewer.rdylgn(100)
+#plot(ndwi, col = brewer.pal(11, "RdYlGn"), font.main = 1, main = "NDWI for Landsat-8 OLI/TIRS C1 image LC09_L2SP_173049_20221221_20221224_02_T1_SR_B: \nWhite Nile and Blue Nile confluence, Sudan (2022)", cex.main=0.9)
+plot(ndwi, col = colors, font.main = 1, main = "NDWI for Landsat-8 OLI/TIRS C1 image LC09_L2SP_173049_20221221_20221224_02_T1_SR_B: \nWhite Nile and Blue Nile confluence, Sudan (2022)", cex.main=0.9)
 minor.tick(nx = 10, ny = 10, tick.ratio = 0.3)
-# Plotting histogram of the NDVI
+# Plotting the histogram
 hist(ndwi, font.main = 1, main = "NDWI values for Landsat-8 OLI/TIRS C1 image \nLC09_L2SP_173049_20221221_20221224_02_T1_SR_B: White Nile and Blue Nile confluence, Sudan (2022)", xlab = "NDWI", ylab= "Frequency",
     col = "mediumpurple1", xlim = c(-0.5, 1),  breaks = 50, xaxt = "n")
 axis(side=1, at = seq(-0.6, 1, 0.1), labels = seq(-0.6, 1, 0.1))
